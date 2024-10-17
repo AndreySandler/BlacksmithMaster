3c2b8ac6fcd2dec60420ce0abebcc6d099575214
Andrey Sandler <mrdreem13@gmail.com>
2024-10-17 12:04:19 +0300

    Adding counter label + counter for click

--- BlacksmithMaster/GameScene.swift
+++ BlacksmithMaster/GameScene.swift
@@ -11,16 +11,23 @@ import GameplayKit
 class GameScene: SKScene {
     var anvil: SKSpriteNode?
     var ingot: SKSpriteNode?
+    var counterLabel: SKLabelNode?
+    
+    var counterText = 0
     
     override func didMove(to view: SKView) {
         anvil = self.childNode(withName: "anvil") as? SKSpriteNode
         ingot = self.childNode(withName: "ingot") as? SKSpriteNode
+        counterLabel = self.childNode(withName: "counterLabel") as? SKLabelNode
+        
+        counterLabel?.fontSize = 80
     }
     
 
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        counterText += 1
+        counterLabel?.text = String(counterText)
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
