4b4123e4bd60bdb00dabc059d1b143cf4f26ffa5
Andrey Sandler <mrdreem13@gmail.com>
2024-10-27 15:00:48 +0300

    Test build v1

--- BlacksmithMaster/MiningScene.swift
+++ BlacksmithMaster/MiningScene.swift
@@ -1,34 +1,32 @@
 //
-//  GameScene.swift
+//  MiningScene.swift
 //  BlacksmithMaster
 //
-//  Created by Andrey Sandler on 03.10.2024.
+//  Created by Andrey Sandler on 17.10.2024.
 //
 
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
-    var anvil: SKSpriteNode?
-    var ingot: SKSpriteNode?
-    var miningButton: SKSpriteNode?
+class MiningScene: SKScene {
     var counterLabel: SKLabelNode?
+    var homeButton: SKSpriteNode?
     
     var counterText = 0
     
     override func didMove(to view: SKView) {
-        anvil = self.childNode(withName: "anvil") as? SKSpriteNode
-        ingot = self.childNode(withName: "ingot") as? SKSpriteNode
-        miningButton = self.childNode(withName: "miningButton") as? SKSpriteNode
         counterLabel = self.childNode(withName: "counterLabel") as? SKLabelNode
+        homeButton = self.childNode(withName: "homeButton") as? SKSpriteNode
     }
+    
+
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
             let touchedNode = self.nodes(at: location)
-            if touchedNode.first?.position == miningButton?.position {
-                let newScene = SKScene(fileNamed: "MiningScene")
+            if touchedNode.first?.position == homeButton?.position {
+                let newScene = SKScene(fileNamed: "GameScene")
                 newScene?.scaleMode = .aspectFill
                 scene?.view?.presentScene(newScene!, transition: .crossFade(withDuration: 1))
             } else {
@@ -39,14 +37,14 @@ class GameScene: SKScene {
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+       
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+
     }
-    
+
     override func update(_ currentTime: TimeInterval) {
-        
+        // Called before each frame is rendered
     }
 }
