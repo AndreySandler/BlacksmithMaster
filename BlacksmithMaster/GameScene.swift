1266eb5000889ff26e5d19cff9256aadea294790
Andrey Sandler <mrdreem13@gmail.com>
2024-10-29 17:13:55 +0300

    Some new sprites, testing ingots state and more more more...test build soon

--- BlacksmithMaster/GameScene.swift
+++ BlacksmithMaster/GameScene.swift
@@ -12,23 +12,27 @@ class GameScene: SKScene {
     
     // MARK: - SpriteKit Nodes
     var anvil: SKSpriteNode?
-    var ingot: SKSpriteNode?
     var miningButton: SKSpriteNode?
     var warehouseButton: SKSpriteNode?
     var counterLabel: SKLabelNode?
     
     // MARK: - Public properties
     var counterText = 0
+    let ingot = SKSpriteNode(imageNamed: "ingot")
+    
     
     // MARK: - Override functions
     override func didMove(to view: SKView) {
         anvil = self.childNode(withName: "anvil") as? SKSpriteNode
-        ingot = self.childNode(withName: "ingot") as? SKSpriteNode
         miningButton = self.childNode(withName: "miningButton") as? SKSpriteNode
         warehouseButton = self.childNode(withName: "warehouseButton") as? SKSpriteNode
         counterLabel = self.childNode(withName: "counterLabel") as? SKLabelNode
-
+        
+        ingot.size = CGSize(width: 128, height: 128)
+        ingot.zPosition = 3
         counterLabel?.text = String(counterText)
+        ingot.position = CGPoint(x: 0, y: 45)
+        self.addChild(ingot)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -40,7 +44,7 @@ class GameScene: SKScene {
                 let miningScene = MiningScene(fileNamed: "MiningScene")
                 
                 miningScene?.counterText = counterText
-                miningScene?.scaleMode = .aspectFill
+                miningScene?.scaleMode = .aspectFit
                 scene?.view?.presentScene(miningScene!, transition: .fade(withDuration: 1))
             } else if touchedNode.first?.position == warehouseButton?.position {
                 let warehouseScene = WarehouseScene(fileNamed: "WarehouseScene")
@@ -51,6 +55,19 @@ class GameScene: SKScene {
             } else {
                 counterText += 1
                 counterLabel?.text = String(counterText)
+                if counterText >= 10 && counterText <= 29 {
+                    ingot.run(SKAction.setTexture(SKTexture(imageNamed: "ingotOne")))
+                    ingot.size = CGSize(width: 128, height: 128)
+                } else if counterText >= 30 && counterText <= 49 {
+                    ingot.run(SKAction.setTexture(SKTexture(imageNamed: "ingotTwo")))
+                    ingot.size = CGSize(width: 128, height: 128)
+                } else if counterText >= 50 && counterText <= 99 {
+                    ingot.run(SKAction.setTexture(SKTexture(imageNamed: "ingotThree")))
+                    ingot.size = CGSize(width: 128, height: 128)
+                } else if counterText >= 100 {
+                    ingot.run(SKAction.setTexture(SKTexture(imageNamed: "sword")))
+                    ingot.size = CGSize(width: 128, height: 128)
+                }
             }
         }
     }
